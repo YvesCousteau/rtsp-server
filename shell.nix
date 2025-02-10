f9102729cce0198786a278e7db3f1190d68b16b9
YvesCousteau <45556867+YvesCousteau@users.noreply.github.com>
2025-02-10 13:31:28 +0100

    FIX

--- shell.nix
+++ shell.nix
@@ -11,12 +11,14 @@ pkgs.mkShell {
     gst_all_1.gst-plugins-bad
     gst_all_1.gst-plugins-ugly
     gst_all_1.gst-rtsp-server
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
     glib
   ];
 
   shellHook = ''
     export PKG_CONFIG_PATH=${pkgs.gst_all_1.gstreamer.dev}/lib/pkgconfig:${pkgs.gst_all_1.gst-plugins-base.dev}/lib/pkgconfig
-    export LD_LIBRARY_PATH="${
+    export LD_LIBRARY_PATH=${
       pkgs.lib.makeLibraryPath [
         pkgs.gst_all_1.gstreamer
         pkgs.gst_all_1.gst-plugins-base
@@ -24,9 +26,21 @@ pkgs.mkShell {
         pkgs.gst_all_1.gst-plugins-bad
         pkgs.gst_all_1.gst-plugins-ugly
         pkgs.gst_all_1.gst-rtsp-server
+        pkgs.gst_all_1.gst-libav
+        pkgs.gst_all_1.gst-vaapi
         pkgs.glib
       ]
-    }:$LD_LIBRARY_PATH"
-    export GST_PLUGIN_SYSTEM_PATH_1_0="${pkgs.gst_all_1.gstreamer}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0:${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0"
+    }:$LD_LIBRARY_PATH
+    export GST_PLUGIN_SYSTEM_PATH_1_0=${
+      pkgs.lib.makeLibraryPath [
+        pkgs.gst_all_1.gstreamer
+        pkgs.gst_all_1.gst-plugins-base
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
+        pkgs.gst_all_1.gst-libav
+        pkgs.gst_all_1.gst-vaapi
+      ]
+    }
   '';
 }
--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,32 @@ fn main() {
     gstreamer::init().unwrap();
 
     let server = gstreamer_rtsp_server::RTSPServer::default();
+
     let mounts = server.mount_points().unwrap();
 
     let factory = gstreamer_rtsp_server::RTSPMediaFactory::default();
     factory.set_launch(
-        "filesrc location=./test.mp4 ! decodebin ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96"
+        "videotestsrc ! videoconvert ! video/x-raw,width=640,height=480 ! x264enc ! rtph264pay name=pay0 pt=96"
     );
+    println!("Pipeline set up: videotestsrc -> x264enc -> rtph264pay");
 
     factory.connect_media_configure(|_, media| {
         let pipeline = media.element();
+        println!("Media configured: {:?}", pipeline);
+
         let bus = pipeline.bus().unwrap();
         bus.connect_message(Some("eos"), move |_, _| {
+            println!("Received EOS (End of Stream) message. Restarting pipeline...");
             pipeline.set_state(gstreamer::State::Null).unwrap();
             pipeline.set_state(gstreamer::State::Playing).unwrap();
         });
     });
 
     mounts.add_factory("/test", factory);
+    println!("RTSP mount point added at /test");
 
     let _ = server.attach(None);
-    println!("Stream ready at rtsp://127.0.0.1:8554/test");
-
+    println!("Stream ready at rtsp://0.0.0.0:{}/test", server.service());
     let main_loop = glib::MainLoop::new(None, false);
     main_loop.run();
 }
